11fadb3683f7813e81728bd228bfe66ab83db192
Matthias Loibl <mail@matthiasloibl.com>
2018-01-21 20:55:22 +0100

    Add fontawesome package with icon strings

---
+++ fontawesome/fontawesome.go
@@ -0,0 +1,15 @@
+package fontawesome
+
+const (
+	ArrowCircleUp            = "\uF0AA"
+	Chrome                   = "\uF268"
+	Globe                    = "\uF0AC"
+	ThermometerEmpty         = "\uF2CB"
+	ThermometerFull          = "\uF2C7"
+	ThermometerHalf          = "\uF2C9"
+	ThermometerQuarter       = "\uF2CA"
+	ThermometerThreeQuarters = "\uF2C8"
+	VolumeDown               = "\uF027"
+	VolumeOff                = "\uF026"
+	VolumeUp                 = "\uF028"
+)
